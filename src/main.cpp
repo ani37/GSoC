5a08f45b543a87dd9ceb590553e16df9549b8fa4
ani37 <aniketagarwal37@gmail.com>
2021-06-11 22:42:51 +0530

    update

--- src/main.cpp
+++ src/main.cpp
@@ -2,7 +2,8 @@
 #include <algorithm>
 #include <iostream>
 #include <vector>
-
+#include<stdio.h>
+#include "happly.h"
 //Ponca
 #include <Ponca/src/Fitting/basket.h>
 #include <Ponca/src/Fitting/gls.h>
@@ -40,6 +41,7 @@ using namespace geometrycentral::surface;
    In this example, we use this class to map an interlaced raw array containing
    both point normals and coordinates.
  */
+// This class defines the input data format
 class MyPoint
 {
 public:
@@ -60,6 +62,7 @@ private:
     Eigen::Map< const VectorType > m_pos, m_normal;
 };
  
+ 
 typedef MyPoint::Scalar Scalar;
 typedef MyPoint::VectorType VectorType;
  
@@ -75,15 +78,13 @@ VectorType test_fit(Fit& _fit,
               const VectorType& _p)
 {
     Scalar tmax = 100.0;
-    
-    //
-    VectorType norm;
  
     // Set a weighting function instance
     _fit.setWeightFunc(WeightFunc(tmax));
  
     // Set the evaluation position
     _fit.init(_p);
+    VectorType vec;
  
     // Iterate over samples and _fit the primitive
     // A MyPoint instance is generated on the fly to bind the raw arrays to the
@@ -99,118 +100,122 @@ VectorType test_fit(Fit& _fit,
     //Test if the fitting ended without errors
     if(_fit.isStable())
     {
-        cout << "Center: [" << _fit.center().transpose() << "] ;  radius: " << _fit.radius() << endl;
+        // cout << "Center: [" << _fit.center().transpose() << "] ;  radius: " << _fit.radius() << endl;
  
-        cout << "Pratt normalization"
-            << (_fit.applyPrattNorm() ? " is now done." : " has already been applied.") << endl;
+        // cout << "Pratt normalization"
+        //     << (_fit.applyPrattNorm() ? " is now done." : " has already been applied.") << endl;
  
-        // Play with fitting output
-        cout << "Value of the scalar field at the initial point: "
-            << _p.transpose()
-            << " is equal to " << _fit.potential(_p)
-            << endl;
+        // // Play with fitting output
+        // cout << "Value of the scalar field at the initial point: "
+        //     << _p.transpose()
+        //     << " is equal to " << _fit.potential(_p)
+        //     << endl;
  
-        cout << "It's gradient at this place is equal to: "
-            << _fit.primitiveGradient(_p).transpose()
-            << endl;
-        // set normals to return
-        norm = _fit.primitiveGradient(_p).transpose();
+        // cout << "It's gradient at this place is equal to: "
+        //     << _fit.primitiveGradient(_p).transpose()
+        //     << endl;
+        vec = _fit.primitiveGradient(_p).transpose();
  
-        cout << "Fitted Sphere: " << endl
-            << "\t Tau  : "      << _fit.tau()             << endl
-            << "\t Eta  : "      << _fit.eta().transpose() << endl
-            << "\t Kappa: "      << _fit.kappa()           << endl;
+        // cout << "Fitted Sphere: " << endl
+        //     << "\t Tau  : "      << _fit.tau()             << endl
+        //     << "\t Eta  : "      << _fit.eta().transpose() << endl
+        //     << "\t Kappa: "      << _fit.kappa()           << endl;
  
-        cout << "The initial point " << _p.transpose()              << endl
-            << "Is projected at   " << _fit.project(_p).transpose() << endl;
+        // cout << "The initial point " << _p.transpose()              << endl
+        //     << "Is projected at   " << _fit.project(_p).transpose() << endl;
     }
-    return norm;
+    return vec;
 }
 
 
  
 // Build an interlaced array containing _n position and normal vectors
-Scalar* buildInterlacedArray(vector<Vector3 > pos, vector<Vector3 > norms )
+Scalar* buildInterlacedArray(std::vector<std::array<Scalar, 3> >  pos )
 {
     int _n = pos.size();
-    Scalar* interlacedArray = new Scalar[uint8_t(2*DIMENSION*_n)];
+    Scalar* interlacedArray = new Scalar[(2*DIMENSION*_n + DIMENSION)];
+
  
     for(int k=0; k<_n; ++k)
     {
-        
-        // Grab position coordinates and store them as raw buffer
-        interlacedArray[2*DIMENSION*k] = pos[k][0];
-        interlacedArray[2*DIMENSION*k+1] = pos[k][1];
-        interlacedArray[2*DIMENSION*k+2] = pos[k][2];
 
-        // Grab normal coordinates and store them as raw buffer
-        interlacedArray[2*DIMENSION*k + DIMENSION ] = norms[k][0];
-        interlacedArray[2*DIMENSION*k + DIMENSION + 1] =norms[k][1];
-        interlacedArray[2*DIMENSION*k + DIMENSION + 2] = norms[k][2];
+        //cout << pos[k][0] << " " << pos[k][1] <<' ' << pos[k][2] << '\n';
+        // Grab position coordinates and store them as raw buffer
+        interlacedArray[(2*DIMENSION*k)] = pos[k][0];
+        interlacedArray[(2*DIMENSION*k+1)] = pos[k][1];
+        interlacedArray[(2*DIMENSION*k+2)] = pos[k][2];
+
+       // Grab normal coordinates and store them as raw buffer
+        interlacedArray[(2*DIMENSION*k + DIMENSION) ] = 0;
+        interlacedArray[(2*DIMENSION*k + DIMENSION + 1)] = 0;
+        interlacedArray[(2*DIMENSION*k + DIMENSION + 2)] = 0;
         
     }
  
     return interlacedArray;
+}
+
+void loadPolygonSoup_PLY(std::string filename, std::vector<std::array<double, 3>>& vertexPositionsOut,
+                         std::vector<std::vector<size_t>>& faceIndicesOut) {
+
+  happly::PLYData plyIn(filename);
+
+  // Get mesh-style data from the object
+  vertexPositionsOut = plyIn.getVertexPositions();
+  //faceIndicesOut = plyIn.getFaceIndices<size_t>();
+
 }
  
 
 int main(int argc, char **argv) {
 
    
-
+    freopen("output.txt", "w", stdout);
     polyscope::init();
 
-    std::unique_ptr<SurfaceMesh> mesh;
-    std::unique_ptr<VertexPositionGeometry> geometry;
-    std::tie(mesh, geometry) = readSurfaceMesh("hippo.ply");
-    geometry->requireVertexPositions();
+    // std::unique_ptr<SurfaceMesh> mesh;
+    // std::unique_ptr<VertexPositionGeometry> geometry;
+    //std::tie(mesh, geometry) = readManifoldSurfaceMesh("hippo.ply");
+    string filename = "hippo.ply";
 
-
-    std::vector<Vector3> pos, norms;
-
-
-    // Build the node positions
-    for (Face f : mesh->faces()) {
-
-    // Compute center for face
-    Vector3 c = Vector3::zero();
-    for (Vertex v : f.adjacentVertices()) {
-        c += geometry->inputVertexPositions[v];
+    std::ifstream testStream(filename);
+    if (!testStream) {
+        return 0;
     }
-    c /= f.degree();
+    std::vector<std::array<Scalar, 3> > positions;
+    std::vector<std::vector<size_t>> faceIndicesOut;
+    loadPolygonSoup_PLY(filename, positions, faceIndicesOut);
+    testStream.close();
 
-    pos.push_back(c);
-    }
+    int n = positions.size();
+   
+    // //Find normals for each positions in test fit.
+    std::vector<std::array<Scalar, 3> >  normals;
 
-    // Set vertex tangent spaces
-    geometry->requireVertexTangentBasis();
-    for(Vertex v : mesh->vertices()) {
-        norms.push_back(geometry->vertexTangentBasis[v][0]);
-    }
+    Scalar *interlacedArray = buildInterlacedArray(positions);
 
-    Scalar *interlacedArray = buildInterlacedArray(pos, norms);
-
-    int n = pos.size();
-    // Find normals for each positions in test fit.
-
-    vector<VectorType> normals;
+    cout <<"\n\n";
     int dim = 0;
+
     for(int i = 0; i < n; i++){
 
         // set evaluation point and scale at the first coordinate
-        VectorType p (interlacedArray + dim);
+        VectorType p ({positions[i][0], positions[i][1],positions[i][2] });
         
         // Here we now perform the fit, starting from a raw interlaced buffer, without
         // any data duplication
         Fit fit;
-        VectorType  n = test_fit(fit, interlacedArray, pos.size(), p);
-        normals.push_back(n);
-        dim += 2*DIMENSION;
+        VectorType  no = test_fit(fit, interlacedArray, n, p);
+        
+        cout << no <<   "\n\n";
+        
+        normals.push_back({no[0], no[1], no[2]});   
+       
+        dim += 2*DIMENSION; 
     }
     
-    
     // visualize!
-    // polyscope::registerPointCloud("positions", pos);
-    // polyscope::registerPointCloud("normals", norms);
-    // polyscope::show();
+      polyscope::registerPointCloud("positions", positions);
+      polyscope::registerPointCloud("normals", normals);
+      polyscope::show();
 }

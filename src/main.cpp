7c0f4d639171eb9fb23bd0f3067f249e0b967d3a
ani37 <aniketagarwal37@gmail.com>
2021-06-17 13:13:26 +0530

    updated with KnearestKdtree

--- src/main.cpp
+++ src/main.cpp
@@ -65,9 +65,9 @@ public:
  
 private:
     Eigen::Map< const VectorType > m_pos, m_normal;
-};
- 
- 
+}; 
+
+
 typedef MyPoint::Scalar Scalar;
 typedef MyPoint::VectorType VectorType;
  
@@ -100,6 +100,7 @@ int main(int argc, char **argv) {
     if (!testStream) {
         return 0;
     }
+
     std::vector<std::array<Scalar, 3> > positions;
 
     // not required here
@@ -109,56 +110,53 @@ int main(int argc, char **argv) {
     testStream.close();
 
     int n = positions.size();
-    std::vector<VectorType >  normals(n);
+    std::vector<std::array<Scalar, 3> >  normals(n);
     
 
     vector<MyPoint> points;
     for(int i = 0; i < n; i++){
         points.push_back({positions[i], {0,0,0}});
+
     }
-
-    Scalar tmax = 0.2;
-
+   
 	KdTree<MyPoint> structure(points);
-
-
-    int dim = 0;
+    
     for(int i = 0; i < n; i++){
 
         // set evaluation point and scale at the ith coordinate
+        Scalar tmax = Eigen::internal::random<Scalar>(10, 25);
         
         const VectorType& p = points.at(i).pos();
 	
         // Here we now perform the fit
         Fit fit;
-        fit.init(p);
-
         // Set a weighting function instance
         fit.setWeightFunc(WeightFunc(tmax));
-    
-        // Iterate over samples and _fit the primitive
-        // A MyPoint instance is generated on the fly to bind the raw arrays to the
-        // library representation. No copy is done at this step.
+        // Set the evaluation position
+        fit.init(p);
+
 
         vector<MyPoint> pos;
-        for (int j : structure.range_neighbors(p, tmax)) {
-			pos.push_back(points[i]);
+        for (const int &j : structure.k_nearest_neighbors(p, tmax)) {
+			pos.push_back(points[j]);
+       
 		}
+       
+
        
         //test_fit(fit, points, structure.range_neighbors(p, tmax), tmax);
         fit.compute( pos.begin(), pos.end() );
         
-        //finalize fitting
-        fit.finalize();
         
         if(fit.isStable()){
-        normals[i] = fit.primitiveGradient(p).transpose();
+            VectorType no = fit.primitiveGradient(p).transpose();
+            normals[i] = {no[0],no[1], no[2]};
         }
-        
     }
-    
+
     // visualize!
       polyscope::registerPointCloud("positions", positions);
-      polyscope::registerPointCloud("normals", normals);
+      polyscope::getPointCloud("positions")->addVectorQuantity("normals", normals);
+      
       polyscope::show();
 }
